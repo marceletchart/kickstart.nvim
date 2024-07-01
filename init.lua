6095afea6ba87920d1bb2c9597c4b2a209598bfa
Marcel Etchart <marcel@cybergate.studio>
2024-07-01 13:09:09 -0400

    add tabnine support

--- init.lua
+++ init.lua
@@ -400,7 +400,6 @@ require('lazy').setup({
       end, { desc = '[S]earch [N]eovim files' })
     end,
   },
-
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -756,6 +755,7 @@ require('lazy').setup({
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         },
         sources = {
+          { name = 'tabnine' },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
